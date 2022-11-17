e5b9ab3bed1887c5d16f739736d72b98ff864255
Swag Finger <swagfinger@gmail.com>
2022-11-18 01:35:58 +0800

    swiping

--- src/Deck.js
+++ src/Deck.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { PanResponder, Animated, View, Dimensions } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -6,21 +6,22 @@ const SCALE = 1.5;
 const SWIPE_THRESHOLD = SCREEN_WIDTH * 0.25; //distance card needs to be dragged
 const SWIPEOUT_DURATION = 250; //milliseconds
 
-const Deck = ({ renderCard, data }) => {
+const Deck = ({
+  renderCard,
+  data,
+  onSwipeLeft = () => {},
+  onSwipeRight = () => {}
+}) => {
+  //track card index
+  const [index, setIndex] = useState(0);
+  console.log('index: ', index);
   //OPTIMIZATION: useRef so same reference on re-render
   const position = useRef(new Animated.ValueXY()).current;
+
   const panResponder = useRef(
     PanResponder.create({
       //user taps/presses down on screen AND should this pan responder handle the event
       onStartShouldSetPanResponder: () => true,
-
-      // onMoveShouldSetPanResponder: () => true,
-      // onPanResponderGrant: () => {
-      //   pan.setOffset({
-      //     x: pan.x._value,
-      //     y: pan.y._value
-      //   });
-      // },
 
       //when user starts dragging. gesture has info about what user is doing with finger, and speed its moving
       //when user stops dragging, gesture becomes 0 unless saved
@@ -48,7 +49,7 @@ const Deck = ({ renderCard, data }) => {
     })
   ).current;
 
-  function getCardStyle() {
+  const getCardStyle = () => {
     //interpolates goal is to tie input (x) with output (rotation)
     //association of one scale, with another scale: input vs output
     //Dimensions gets dimensions of screen
@@ -61,24 +62,47 @@ const Deck = ({ renderCard, data }) => {
       ...position.getLayout(),
       transform: [{ rotate }]
     };
-  }
+  };
 
   const forceSwipe = (direction) => {
     const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
     //duration is millisec
     Animated.timing(position, {
       toValue: { x, y: 0 },
-      duration: SWIPEOUT_DURATION
+      duration: SWIPEOUT_DURATION,
+      useNativeDriver: false
+    }).start(() => {
+      //executes after animation completed
+      onSwipeComplete(direction);
+    });
+  };
+
+  const onSwipeComplete = (direction) => {
+    console.log({ index });
+    const item = data[index]; //the item just swiped
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+
+    position.setValue({ x: 0, y: 0 });
+
+    setIndex((prev) => {
+      return prev + 1;
+    });
+  };
+
+  const resetPosition = () => {
+    Animated.spring(position, {
+      toValue: { x: 0, y: 0 },
+      useNativeDriver: false
     }).start();
   };
 
-  function resetPosition() {
-    Animated.spring(position, { toValue: { x: 0, y: 0 } }).start();
-  }
-
   const renderCards = () => {
-    return data.map((item, index) => {
-      if (index === 0) {
+    return data.map((item, i) => {
+      if (i < index) {
+        return null;
+      }
+      if (index === i) {
         return (
           <Animated.View
             key={item.id}
